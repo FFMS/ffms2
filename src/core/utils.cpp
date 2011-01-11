1dc1953194b0c6f40f3e35815f53c5ba1b86c0fa
Thomas Goyne <tgoyne@gmail.com>
2011-01-11 05:38:31 +0000

    Don't include files needed on all platforms inside a #ifdef _WIN32 block. Patch by wipple625. Closes issue #38.
    Originally committed to SVN as r411.

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -30,10 +30,6 @@
 #	include <fcntl.h>
 extern "C" {
 #	include "libavutil/avstring.h"
-	/* if you have this, we'll assume you have a new enough libavutil too */
-#	if LIBSWSCALE_VERSION_INT >= AV_VERSION_INT(0, 12, 0)
-#		include <libavutil/opt.h>
-#	endif
 }
 #endif // _WIN32
 
@@ -49,6 +45,11 @@ extern const AVCodecTag ff_codec_bmp_tags[];
 extern const CodecTags ff_mkv_codec_tags[];
 extern const AVCodecTag ff_codec_movvideo_tags[];
 extern const AVCodecTag ff_codec_wav_tags[];
+
+/* if you have this, we'll assume you have a new enough libavutil too */
+#if LIBSWSCALE_VERSION_INT >= AV_VERSION_INT(0, 12, 0)
+#	include <libavutil/opt.h>
+#endif
 }
 
 extern int CPUFeatures;
