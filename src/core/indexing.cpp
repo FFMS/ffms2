4a626a46c5a8b0c75cd2886007d2fb8ca7d312de
Thomas Goyne <tgoyne@gmail.com>
2011-05-03 04:12:49 +0000

    Fix compilation with older versions of ffmpeg
    Originally committed to SVN as r461.

--- src/core/indexing.cpp
+++ src/core/indexing.cpp
@@ -27,7 +27,18 @@
 
 
 extern "C" {
+#include <libavutil/avutil.h>
+
+#if LIBAVUTIL_VERSION_INT > AV_VERSION_INT(50, 40, 1)
 #include <libavutil/sha.h>
+#else
+extern const int av_sha_size;
+struct AVSHA;
+int av_sha_init(struct AVSHA* context, int bits);
+void av_sha_update(struct AVSHA* context, const uint8_t* data, unsigned int len);
+void av_sha_final(struct AVSHA* context, uint8_t *digest);
+#endif
+
 #include <zlib.h>
 }
 
