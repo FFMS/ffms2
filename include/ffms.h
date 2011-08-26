e345210cd2a198f496339df523f4fbd62cab67e0
Kalle Blomster <kalle.blomster@gmail.com>
2011-08-26 20:11:09 +0000

    bump the version number to 2.16.0.0
    Originally committed to SVN as r543.

--- include/ffms.h
+++ include/ffms.h
@@ -22,7 +22,7 @@
 #define FFMS_H
 
 // Version format: major - minor - micro - bump
-#define FFMS_VERSION ((2 << 24) | (15 << 16) | (4 << 8) | 2)
+#define FFMS_VERSION ((2 << 24) | (16 << 16) | (0 << 8) | 0)
 
 #include <stdint.h>
 
