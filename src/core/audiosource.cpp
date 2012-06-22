036ebe09d9ca654305aeb8ef4408f33b4b5facd6
Thomas Goyne <tgoyne@gmail.com>
2012-06-22 21:21:56 +0000

    Fix handling of audio delay with invalid inital timestamps
    When the first audio PTS in a file is AV_NOPTS_VALUE, it was overflowing
    int64_t, resulting in the delay being 0 with MSVC, and possibly other
    values with other compiles. To fix this, use the first valid PTS and
    calculate the elapsed time before that packet from the sample counts.
    
    Originally committed to SVN as r700.

--- src/core/audiosource.cpp
+++ src/core/audiosource.cpp
@@ -118,7 +118,7 @@ void FFMS_AudioSource::Init(const FFMS_Index &Index, int DelayMode) {
 		throw FFMS_Exception(FFMS_ERROR_INDEX, FFMS_ERROR_INVALID_ARGUMENT,
 			"Audio delay compensation must be relative to a video track");
 
-	double AdjustRelative = 0;
+	int64_t Delay = 0;
 	if (DelayMode != FFMS_DELAY_TIME_ZERO) {
 		if (DelayMode == FFMS_DELAY_FIRST_VIDEO_TRACK) {
 			for (size_t i = 0; i < Index.size(); ++i) {
@@ -131,11 +131,18 @@ void FFMS_AudioSource::Init(const FFMS_Index &Index, int DelayMode) {
 
 		if (DelayMode >= 0) {
 			const FFMS_Track &VTrack = Index[DelayMode];
-			AdjustRelative = VTrack[0].PTS * VTrack.TB.Num / (double)VTrack.TB.Den;
+			Delay = -(VTrack[0].PTS * VTrack.TB.Num * AP.SampleRate / (VTrack.TB.Den * 1000));
 		}
 	}
 
-	Delay = static_cast<int64_t>((Frames[0].PTS * Frames.TB.Num / (double)Frames.TB.Den - AdjustRelative) * AP.SampleRate / 1000. + .5);
+	if (Frames.HasTS) {
+		int i = 0;
+		while (Frames[i].PTS == ffms_av_nopts_value) ++i;
+		Delay += Frames[i].PTS * Frames.TB.Num * AP.SampleRate / (Frames.TB.Den * 1000);
+		for (; i >= 0; --i)
+			Delay -= Frames[i].SampleCount;
+	}
+
 	AP.NumSamples += Delay;
 }
 
