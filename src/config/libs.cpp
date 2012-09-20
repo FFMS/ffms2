71b50bddb773b0a16bf4829e6f987921580256c4
Alex Smith <asmith5152@gmail.com>
2012-09-20 12:39:38 +0000

    msvc: Add support for libav/ffmpeg compiled with msvc.
    By default when building ffms2 with msvc it will assume that
    libav/ffmpeg were also built with msvc.  This avoids the various
    problems of linking against mingw static libraries in msvc and makes the
    world a little saner.
    
    Also includes support for opencore-amr built with msvc.  Support for
    libraries other than opencore-amr may be added later.
    
    Removes support for the autodetection of libraries which can now be
    error prone with the additional msvc support in libav/ffmpeg.
    Additional libraries must now be manually enabled in msvc-config.h.
    
    Originally committed to SVN as r714.

--- src/config/libs.cpp
+++ src/config/libs.cpp
@@ -25,36 +25,52 @@ extern "C" {
 }
 
 // someone claimed that the borland c++ compiler defines _MSC_VER too,
-// but nobody cares about borland so let them suffer
+// but nobody cares about borland so let them suffer.
 #ifdef _MSC_VER
 
 
-// standard mingw/ffmpeg libraries
+#ifdef WITH_GCC_LIBAV
+// Standard mingw libraries.
 #pragma comment(lib, "libgcc.a")
 #pragma comment(lib, "libmoldname.a")
 #pragma comment(lib, "libmingwex.a")
 #pragma comment(lib, "libz.a")
+#pragma comment(lib, "libwsock32.a")
+#else
+#pragma comment(lib, "zlib.lib")
+#endif /* WITH_GCC_LIBAV */
+
+// libav/ffmpeg libs are the same (name) regardless of their compiler.
 #pragma comment(lib, "libavutil.a")
 #pragma comment(lib, "libavcodec.a")
 #pragma comment(lib, "libavformat.a")
 #pragma comment(lib, "libswscale.a")
-#pragma comment(lib, "libwsock32.a")
 
 #if (LIBAVUTIL_VERSION_INT) < (AV_VERSION_INT(50,38,0))
 #	pragma comment(lib, "libavcore.a")
 #endif
 
 #ifdef WITH_OPENCORE_AMR_NB
+#ifdef WITH_GCC_LIBAV
 #pragma comment(lib, "libopencore-amrnb.a")
-#endif // WITH_OPENCORE_AMR_NB
+#else
+#pragma comment(lib, "opencore-amrnb.lib")
+#endif /* WITH_GCC_LIBAV */
+#endif /* WITH_OPENCORE_AMR_NB */
 
 #ifdef WITH_OPENCORE_AMR_WB
+#ifdef WITH_GCC_LIBAV
 #pragma comment(lib, "libopencore-amrwb.a")
-#endif // WITH_OPENCORE_AMR_WB
+#else
+#pragma comment(lib, "opencore-amrwb.lib")
+#endif /* WITH_GCC_LIBAV */
+#endif /* WITH_OPENCORE_AMR_WB */
 
-#ifdef WITH_PTHREAD_GC2
+#if defined(WITH_PTHREAD_GC2) && defined(WITH_GCC_LIBAV)
 #pragma comment(lib, "libpthreadGC2.a")
-#endif // WITH_PTHREAD_GC2
+#elif defined(WITH_PTHREAD_GC2) && !defined(WITH_GCC_LIBAV)
+#error pthreads is only supported if libav/ffmpeg was built with gcc.
+#endif /* defined(WITH_PTHREAD_GC2) && defined(WITH_GCC_LIBAV) */
 
 
-#endif // _MSC_VER
+#endif /* _MSC_VER */
