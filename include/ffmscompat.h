2590b7bc67aaff10dcefc568d4b4d1d9bdd512e5
Alex Smith <asmith5152@gmail.com>
2012-09-23 22:10:59 +0000

    Fix mingw-w64 compilation.
    Originally committed to SVN as r723.

--- include/ffmscompat.h
+++ include/ffmscompat.h
@@ -39,7 +39,7 @@
 #  define VERSION_CHECK(LIB, cmp, major, minor, micro, u1, u2, u3) ((LIB) cmp (AV_VERSION_INT(major, minor, micro)))
 #endif
 
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(__MINGW64_VERSION_MAJOR)
 #	define snprintf _snprintf
 #	ifdef __MINGW32__
 #		define fseeko fseeko64
