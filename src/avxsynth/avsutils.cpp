06a407479ec61c66afd08d312b351c6d335093d8
Stephen Hutchinson <qyot27@gmail.com>
2015-05-05 13:09:56 -0400

    avxsynth: More c++ and less stricmp
    Port of 5df6f171 to the AvxSynth plugin.

--- src/avxsynth/avsutils.cpp
+++ src/avxsynth/avsutils.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2009 Fredrik Mellbin
+//  Copyright (c) 2009-2015 Fredrik Mellbin
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -19,6 +19,8 @@
 //  THE SOFTWARE.
 
 #include "avsutils.h"
+#include <string>
+#include <algorithm>
 
 extern "C" {
 #include <libswscale/swscale.h>
@@ -27,41 +29,49 @@ extern "C" {
 #define _stricmp strcasecmp
 
 PixelFormat CSNameToPIXFMT(const char *CSName, PixelFormat Default) {
-	if (!_stricmp(CSName, ""))
+	if (!CSName)
+		return PIX_FMT_NONE;
+	std::string s = CSName;
+	std::transform(s.begin(), s.end(), s.begin(), toupper);
+	if (s == "")
 		return Default;
-	if (!_stricmp(CSName, "YV12"))
+	if (s == "YV12")
 		return PIX_FMT_YUV420P;
-	if (!_stricmp(CSName, "YUY2"))
+	if (s == "YUY2")
 		return PIX_FMT_YUYV422;
-	if (!_stricmp(CSName, "RGB24"))
+	if (s == "RGB24")
 		return PIX_FMT_BGR24;
-	if (!_stricmp(CSName, "RGB32"))
+	if (s == "RGB32")
 		return PIX_FMT_RGB32;
 	return PIX_FMT_NONE;
 }
 
 int ResizerNameToSWSResizer(const char *ResizerName) {
-	if (!_stricmp(ResizerName, "FAST_BILINEAR"))
+	if (!ResizerName)
+		return 0;
+	std::string s = ResizerName;
+	std::transform(s.begin(), s.end(), s.begin(), toupper);
+	if (s == "FAST_BILINEAR")
 		return SWS_FAST_BILINEAR;
-	if (!_stricmp(ResizerName, "BILINEAR"))
+	if (s == "BILINEAR")
 		return SWS_BILINEAR;
-	if (!_stricmp(ResizerName, "BICUBIC"))
+	if (s == "BICUBIC")
 		return SWS_BICUBIC;
-	if (!_stricmp(ResizerName, "X"))
+	if (s == "X")
 		return SWS_X;
-	if (!_stricmp(ResizerName, "POINT"))
+	if (s == "POINT")
 		return SWS_POINT;
-	if (!_stricmp(ResizerName, "AREA"))
+	if (s == "AREA")
 		return SWS_AREA;
-	if (!_stricmp(ResizerName, "BICUBLIN"))
+	if (s == "BICUBLIN")
 		return SWS_BICUBLIN;
-	if (!_stricmp(ResizerName, "GAUSS"))
+	if (s == "GAUSS")
 		return SWS_GAUSS;
-	if (!_stricmp(ResizerName, "SINC"))
+	if (s == "SINC")
 		return SWS_SINC;
-	if (!_stricmp(ResizerName, "LANCZOS"))
+	if (s == "LANCZOS")
 		return SWS_LANCZOS;
-	if (!_stricmp(ResizerName, "SPLINE"))
+	if (s == "SPLINE")
 		return SWS_SPLINE;
 	return 0;
 }
