a6245947fe1d39fc0eadfafa5301598373d2235c
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2023-09-29 19:22:20 +0200

    More audio decoding loop changes to better match indexing (which is completely super correct)

--- src/core/audiosource.cpp
+++ src/core/audiosource.cpp
@@ -329,15 +329,7 @@ int FFMS_AudioSource::DecodeNextBlock(CacheIterator *pos) {
                 if (pos)
                     CachedBlock = CacheBlock(*pos);
             }
-            break;
-        } else if (Ret == AVERROR(EAGAIN)) {
-            if (!ReadPacket(Packet)) {
-                av_packet_free(&Packet);
-                throw FFMS_Exception(FFMS_ERROR_PARSER, FFMS_ERROR_UNKNOWN,
-                    "ReadPacket unexpectedly failed to read a packet");
-            }
-            avcodec_send_packet(CodecContext, Packet);
-        } else if (Ret == AVERROR_EOF) {
+        } else if (Ret == AVERROR_EOF || Ret == AVERROR(EAGAIN)) {
             break;
         } else {
             throw FFMS_Exception(FFMS_ERROR_CODEC, FFMS_ERROR_DECODING, "Audio decoding error");
