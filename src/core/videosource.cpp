FFMS/ffms2

commit 6467a17d70f5a2fc978b748a87c8ae2f08bc0a9c

Force a seek right after initializing source filter
The Seek(0) in the FFMS_VideoSource constructor can break
the invariants assumed during the GetFrame loop later on when
the first frame in output order is not the first frame in decoding
order. An example for this is 3d-parrot.MTS from the doom9 collection.
We could try to ensure that this initial seek behaves just like the
future seeks in the GetFrame loop do, but it's simpler to just force
a seek at the start to go into decoding with a clean slate.

Similarly, don't output the frame we decoded in the constructor,
just to be sure.

src/core/videosource.cpp
@@ -700,7 +700,8 @@ int FFMS_VideoSource::Seek(int n) {
     int ret = -1;
 
     Delay.Reset();
-    Stage = DecodeStage::INITIALIZE;
+    if (Stage != DecodeStage::INITIALIZE_SOURCE)
+        Stage = DecodeStage::INITIALIZE;
 
     if (!SeekByPos || Frames[n].FilePos < 0) {
         ret = av_seek_frame(FormatContext, VideoTrack, Frames[n].PTS, AVSEEK_FLAG_BACKWARD);
@@ -819,6 +820,12 @@ void FFMS_VideoSource::DecodeNextFrame(int64_t &AStartTime, int64_t &Pos) {
 }
 
 bool FFMS_VideoSource::SeekTo(int n, int SeekOffset) {
+    bool ForceSeek = false;
+    if (Stage == DecodeStage::INITIALIZE_SOURCE) {
+        ForceSeek = true;
+        Stage = DecodeStage::INITIALIZE;
+    }
+
     // The semantics here are basically "return true if we don't know exactly where our seek ended up (destination isn't frame 0)"
     if (SeekMode >= 0) {
         int TargetFrame = n + SeekOffset;
@@ -847,7 +854,7 @@ bool FFMS_VideoSource::SeekTo(int n, int SeekOffset) {
             }
         } else {
             // 10 frames is used as a margin to prevent excessive seeking since the predicted best keyframe isn't always selected by avformat
-            if (n < CurrentFrame || TargetFrame > CurrentFrame + 10 || (SeekMode == 3 && n > CurrentFrame + 10)) {
+            if (ForceSeek || n < CurrentFrame || TargetFrame > CurrentFrame + 10 || (SeekMode == 3 && n > CurrentFrame + 10)) {
                 Seek(TargetFrame);
                 return true;
             }
@@ -863,7 +870,7 @@ FFMS_Frame *FFMS_VideoSource::GetFrame(int n) {
     GetFrameCheck(n);
     n = Frames.RealFrameNumber(n);
 
-    if (LastFrameNum == n)
+    if (Stage != DecodeStage::INITIALIZE_SOURCE && LastFrameNum == n)
         return &LocalFrame;
 
     int SeekOffset = 0;

src/core/videosource.h
@@ -40,6 +40,7 @@ extern "C" {
 #include "utils.h"
 
 enum class DecodeStage {
+    INITIALIZE_SOURCE,
     INITIALIZE,
     APPLY_DELAY,
     DECODE_LOOP,
@@ -63,7 +64,7 @@ private:
     SwsContext *SWS = nullptr;
 
     DecoderDelay Delay;
-    DecodeStage Stage = DecodeStage::INITIALIZE;
+    DecodeStage Stage = DecodeStage::INITIALIZE_SOURCE;
 
     int LastFrameHeight = -1;
     int LastFrameWidth = -1;

commit 25cef14386fcaaa58ee547065deee8f6e82c56a2

videosource: Do not force a seek on init if we can't seek
This was a regression introduced in 6467a17d70f5a2fc978b748a87c8ae2f08bc0a9c.

Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

src/core/videosource.cpp
@@ -408,6 +408,11 @@ FFMS_VideoSource::FFMS_VideoSource(const char *SourceFile, FFMS_Index &Index, in
             }
         }
 
+        // If we are in linear no-rewind mode, don't attempt to seek during init.
+        // Bypass the INITIALIZE_SOURCE stage.
+        if (SeekMode < 0)
+            Stage = DecodeStage::INITIALIZE;
+
         // Cannot "output" without doing all other initialization
         // This is the additional mess required for seekmode=-1 to work in a reasonable way
         OutputFrame(DecodeFrame);